ffa65791c22ae87b8a4a6c701792343c4efbeed5
Ford Fowler <fordjfowler@gmail.com>
2016-10-26 10:38:40 -0700

    template project html, add projectData file, begin DOM work

---
+++ project.js
@@ -0,0 +1,7 @@
+var projects = [];
+
+function Project(obj) {
+  this.title = obj.title;
+  this.imgsrc = obj.imgsrc;
+  this.caption = obj.caption;
+}
---
+++ projectData.js
@@ -0,0 +1,23 @@
+var projectData = [
+  {
+    title: 'BusMall',
+    imgsrc: 'http://placehold.it/350x150',
+    caption: 'Product research quiz #marketing'
+  },
+  {
+    title: 'WhichBin',
+    imgsrc: 'http://placehold.it/350x150',
+    caption: 'Can\'t decide which bin to put your junk in? Ask WhichBin™!'
+  },
+  {
+    title: 'Salmon Cookies',
+    imgsrc: 'http://placehold.it/350x150',
+    caption: 'One part company homepage, one part backend sales tool'
+  }
+];
+
+// {
+//   title: 'WhichBin',
+//   imgsrc: 'http://placehold.it/350x150',
+//   caption: ''
+// },
