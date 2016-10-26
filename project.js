fa0572a0749dd8f1a9fe6d05be76c4b4ebf1bac3
Ford Fowler <fordjfowler@gmail.com>
2016-10-26 10:55:06 -0700

    toHTML function, initializing loops

--- project.js
+++ project.js
@@ -4,4 +4,24 @@ function Project(obj) {
   this.title = obj.title;
   this.imgsrc = obj.imgsrc;
   this.caption = obj.caption;
-}
+  this.date = obj.date;
+};
+
+Project.prototype.toHTML = function() {
+  var $newProject = $('li.template').clone();
+  $newProject.find('h3').text = this.title;
+  $newProject.find('h6').text = 'Finished: ' + this.date;
+  $newProject.find('img').attr('src', this.imgsrc);
+  $newProject.find('figcaption').text = this.caption;
+
+  $newProject.removeClass();
+  return $newProject;
+};
+
+projectData.forEach(function(proj) {
+  projects.push(new Project(proj));
+});
+
+projects.forEach(function(proj) {
+  $('#projects ul').append(proj.toHTML());
+});
