aac2adf4995e0dd4a989327266c4c88ee7c78a06
Ford Fowler <fordjfowler@gmail.com>
2016-10-27 10:45:42 -0700

    slick stuff

--- scripts/project.js
+++ scripts/project.js
@@ -35,5 +35,7 @@ $projects.slick({
   infinite: true,
   speed: 300,
   slidesToShow: 1,
-  adaptiveHeight: true
+  adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 6000,
 });
--- scripts/projectData.js
+++ scripts/projectData.js
@@ -1,19 +1,19 @@
 var projectData = [
   {
     title: 'BusMall',
-    imgsrc: 'http://placehold.it/700x300/333333',
+    imgsrc: 'http://placehold.it/700x300/000000',
     caption: 'Product research quiz #marketing',
     date: 'October 9th, 2016'
   },
   {
     title: 'WhichBin',
-    imgsrc: 'http://placehold.it/350x150/333333',
+    imgsrc: 'http://placehold.it/700x300/000000',
     caption: 'Can\'t decide which bin to put your junk in? Ask WhichBin™!',
     date: 'October 13th, 2016'
   },
   {
     title: 'Salmon Cookies',
-    imgsrc: 'http://placehold.it/350x150/333333',
+    imgsrc: 'http://placehold.it/700x300/000000',
     caption: 'One part company homepage, one part backend sales tool',
     date: 'October 2nd, 2016'
   }
@@ -21,6 +21,6 @@ var projectData = [
 
 // {
 //   title: 'WhichBin',
-//   imgsrc: 'http://placehold.it/350x150',
+//   imgsrc: 'http://placehold.it/700x300',
 //   caption: ''
 // },
