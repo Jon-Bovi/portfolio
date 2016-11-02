ee958e195ccc54951a6d85515a4a7c0ed4c36e7b
Ford Fowler <fordjfowler@gmail.com>
2016-11-02 11:01:49 -0700

    add local storage functionality, adding more project data

--- scripts/project.js
+++ scripts/project.js
@@ -1,38 +1,60 @@
-function Project(projObj) {
-  this.title = projObj.title;
-  this.imgsrc = projObj.imgsrc;
-  this.caption = projObj.caption;
-  this.date = projObj.date;
-};
+(function(module) {
 
-Project.projects = [];
+  function Project(projObj) {
+    this.title = projObj.title;
+    this.imgsrc = projObj.imgsrc;
+    this.caption = projObj.caption;
+    this.date = projObj.date;
+  };
 
-Project.prototype.toHTML = function() {
-  var template = $('#project-template').html();
-  var templateRender = Handlebars.compile(template);
-  return templateRender(this);
-};
+  Project.projects = [];
 
-Project.loadAll = function(inputData) {
-  inputData.forEach(function(proj) {
-    Project.projects.push(new Project(proj));
-  });
-};
+  Project.prototype.toHTML = function() {
+    var template = $('#project-template').html();
+    var templateRender = Handlebars.compile(template);
+    return templateRender(this);
+  };
 
-Project.fetchAll = function() {
-  // TODO
-    /* When our data is already in localStorage:
-    1. We can process and load it,
-    2. Then we can render the index page.  */
-    /* Without our localStorage in memory, we need to:
-    1. Retrieve our JSON file with $.getJSON
-    1.a Load our json data
-    1.b Store that data in localStorage so that we can skip the server call next time,
-    1.c And then render the index page.*/
-  console.log('Try fetch');
-  $.get('../projects.json', function(data, message, xhr) {
-    console.log('FETCHING');
-    Project.loadAll(data);
-    viewController.render();
-  });
-};
+  Project.loadAll = function(inputData) {
+    inputData.forEach(function(proj) {
+      Project.projects.push(new Project(proj));
+    });
+  };
+
+  Project.fetchAll = function() {
+    $.ajax({
+      type: 'HEAD',
+      url: '../projects.json',
+      success: function(data, message, xhr) {
+        if (!localStorage.eTag || localStorage.eTag !== xhr.getResponseHeader('ETag')) {
+          console.log('load new data');
+          $.getJSON('../projects.json', function(data) {
+            localStorage.setItem('eTag', xhr.getResponseHeader('ETag'));
+            localStorage.setItem('projectData', JSON.stringify(data));
+            Project.loadAll(data);
+            viewController.renderProjects();
+          });
+        } else {
+          console.log('load from local');
+          Project.loadAll(JSON.parse(localStorage.getItem('projectData')));
+          viewController.renderProjects();
+        }
+
+      }
+    });
+    // TODO
+      /* When our data is already in localStorage:
+      1. We can process and load it,
+      2. Then we can render the index page.  */
+      /* Without our localStorage in memory, we need to:
+      1. Retrieve our JSON file with $.getJSON
+      1.a Load our json data
+      1.b Store that data in localStorage so that we can skip the server call next time,
+      1.c And then render the index page.*/
+    console.log('Try fetch');
+
+  };
+
+  module.Project = Project;
+
+})(window);
--- scripts/view.js
+++ scripts/view.js
@@ -1,29 +1,34 @@
-var viewController = {};
+(function(module) {
+  
+  var viewController = {};
 
-viewController.handleNav = function() {
-  $('.navtab').click(function() {
-    $('.navtab-content').hide();
-    which = $(this).attr('data-content');
-    $('#' + which).fadeIn();
-  });
-};
+  viewController.handleNav = function() {
+    $('.navtab').click(function() {
+      $('.navtab-content').hide();
+      which = $(this).attr('data-content');
+      $('#' + which).fadeIn();
+    });
+  };
 
-viewController.handleNav();
+  viewController.renderProjects = function() {
+    $projects = $('.project-carousel');
+    Project.projects.forEach(function(proj) {
+      $projects.append(proj.toHTML());
+    });
+    $projects.slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      autoplay: true,
+      autoplaySpeed: 6000,
+    });
+  };
 
-viewController.render = function() {
-  $projects = $('.project-carousel');
-  Project.projects.forEach(function(proj) {
-    $projects.append(proj.toHTML());
-  });
-  $projects.slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    autoplay: true,
-    autoplaySpeed: 6000,
-  });
-};
+  module.viewController = viewController;
 
-Project.fetchAll();
+  viewController.handleNav();
+  Project.fetchAll();
+
+})(window);
